3ec39916b4f5c029bedc03ec8d776658bd1b0a59
Rahul <rahulsm.1503@gmail.com>
2023-10-15 17:39:24 +0530

    added missing dependency

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,21 +1,3 @@
-// import { prisma } from "../../../prisma/db";
-// export async function POST(req, res) {
-//   const { reporter, datetime, injuries } = await req.json();
-//   try {
-//     await prisma.report.create({
-//       data: {
-//         reporter: reporter,
-//         datetime: datetime,
-//         injuries: injuries,
-//       },
-//     });
-//     return Response.json("Created report successfully");
-//   } catch (err) {
-//     return new Response(`Error: ${err}`, {
-//       status: 404,
-//     });
-//   }
-// }
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { resolvers } from '@/graphql/resolvers';
